4fe2bb652ca206d14845420557b3661a4e29bbdc
Carlos Juan <carlos.techwork@outlook.com>
2025-06-06 21:07:29 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
     return "Hello from Azure App Service!"
 
-print(hello())
+if __name__ == "__main__":
+    app.run()
